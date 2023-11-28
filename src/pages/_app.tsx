ca4ac8d03dc89c9e24562feae3ea39e1426e68ea
Christopher Angelo <angelo@angeloanan.xyz>
2023-11-29 02:58:11 +0800

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../style/globals.css'
 
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import PlausibleProvider from 'next-plausible'
 import { DefaultSeo } from 'next-seo'
 
 import SEOConfig from '../../next-seo.config'
@@ -17,13 +18,19 @@ function CustomApp({ Component, pageProps }: CustomAppProps) {
   return Component.disableLayout ? (
     <Component {...pageProps} />
   ) : (
-    <>
+    <PlausibleProvider
+      domain='fursona.zip'
+      customDomain='https://a.angelo.fyi'
+      selfHosted
+      trackOutboundLinks
+      trackFileDownloads
+    >
       <DefaultSeo {...SEOConfig} />
 
       <div className='stylized-bg min-h-screen'>
         <Component {...pageProps} />
       </div>
-    </>
+    </PlausibleProvider>
   )
 }
 
